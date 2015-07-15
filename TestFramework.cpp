71ddabb27edb39f3fa9fa4e179b4e282b02b8ee5
dan walmsley <dan@walms.co.uk>
2015-07-15 13:42:56 +0100

    initial commit

---
+++ TestFramework.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+*       Description: 
+*
+*       Author: 
+*         Date: 13 July 2015
+*
+*******************************************************************************/
+#pragma mark Compiler Pragmas
+#define CATCH_CONFIG_MAIN
+
+#pragma mark Includes
+#include "Catch.h"
+
+#pragma mark Definitions and Constants
+
+
+#pragma mark Static Data
+
+
+#pragma mark Static Functions
+
+
+#pragma mark Member Implementations
